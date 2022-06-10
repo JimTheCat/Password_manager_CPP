16ef68d2a0c4d56279920d836a240e504c5cb964
Patryk Kłosiński <85301765+JimTheCat@users.noreply.github.com>
2022-06-10 21:25:58 +0200

    edit.cpp TODO

--- Menu/Edytuj/edit.cpp
+++ Menu/Edytuj/edit.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "edit.h"
+
+
+void edit::editPassword(std::vector<std::vector<std::string>> &vec, const std::string& nameOfPassword) {
+    int index = 0;
+    bool passwordFounded = false;
+    for (auto i : vec){
+        if (i[1] == nameOfPassword) {
+            passwordFounded = true;
+            break;
+        }
+        index++;
+    }
+    if(passwordFounded) edit::changingValues(vec, index);
+    else std::cerr << "Nie znaleziono takiego hasla!" << std::endl;
+}
+
+void edit::changingValues(std::vector<std::vector<std::string>> &vec, const int& index){
+    char answer = 'n';
+    while (answer != 't'){
+        const std::string options[] = {"Haslo", "Nazwa", "Strona_WWW", "Login"};
+        int indexOfLine = 1;
+        char choice = '0';
+        std::cout << "Ktory parametr chcialbys zmienic w tym hasle:" << std::endl;
+        for (const auto& i : options){
+            std::cout << indexOfLine++ << ". " << i << std::endl;
+        }
+        //TODO: dokonczyc switcha :3
+//        switch (choice) {
+//            case 1:
+//        }
+
+    }
+}
--- Menu/Edytuj/edit.h
+++ Menu/Edytuj/edit.h
@@ -4,11 +4,14 @@
 
 #ifndef PROJECT_PASSWORD_MANAGER_EDIT_H
 #define PROJECT_PASSWORD_MANAGER_EDIT_H
-
+#include <iostream>
+#include <vector>
 
 class edit {
+public:
+    static void editPassword(std::vector<std::vector<std::string>> &vec, const std::string &nameOfPassword);
 
-};
-
+    static void changingValues(std::vector<std::vector<std::string>> &vec, const int &index);
+}
 
 #endif //PROJECT_PASSWORD_MANAGER_EDIT_H

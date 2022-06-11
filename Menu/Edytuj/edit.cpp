ade436857b6c0cb368e91ca600a325291ae6e905
Patryk Kłosiński <85301765+JimTheCat@users.noreply.github.com>
2022-06-11 12:21:25 +0200

    edit.cpp appears to Password Manager

--- Menu/Edytuj/edit.cpp
+++ Menu/Edytuj/edit.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "edit.h"
+#include "../Dodaj/addPasswd.h"
 
 
 void edit::editPassword(std::vector<std::vector<std::string>> &vec, const std::string& nameOfPassword) {
@@ -16,23 +17,61 @@ void edit::editPassword(std::vector<std::vector<std::string>> &vec, const std::s
         index++;
     }
     if(passwordFounded) edit::changingValues(vec, index);
-    else std::cerr << "Nie znaleziono takiego hasla!" << std::endl;
+    else std::cerr << "void editPassword(): Nie znaleziono takiego hasla!" << std::endl;
 }
 
 void edit::changingValues(std::vector<std::vector<std::string>> &vec, const int& index){
     char answer = 'n';
     while (answer != 't'){
+        std::string textToChange;
         const std::string options[] = {"Haslo", "Nazwa", "Strona_WWW", "Login"};
         int indexOfLine = 1;
         char choice = '0';
+
         std::cout << "Ktory parametr chcialbys zmienic w tym hasle:" << std::endl;
         for (const auto& i : options){
             std::cout << indexOfLine++ << ". " << i << std::endl;
         }
-        //TODO: dokonczyc switcha :3
-//        switch (choice) {
-//            case 1:
-//        }
+        std::cin >> choice;
+        switch (choice) {
+            case '1':{
+                char changePasswd = 'n';
 
+                std::cout << "Czy chcesz randomowe haslo? [t/n]: " << std::endl;
+                std::cin >> changePasswd;
+                if (changePasswd == 't') textToChange = addPasswd::generatePassword();
+                else if (changePasswd == 'n') {
+                    std::cout << "Podaj haslo: " << std::endl;
+                    std::cin >> textToChange;
+                }
+                addPasswd::checkingStrengthOfPassword(textToChange);
+                vec[index][0] = textToChange;
+            }
+            case '2':{
+                bool isNameUnique = false;
+                std::cout << "Podaj nazwe: " << std::endl;
+                while (!isNameUnique) {
+                    std::cin >> textToChange;
+                    isNameUnique = addPasswd::checkingUniqueOfName(textToChange, vec);
+                    if (!isNameUnique) std::cout << "Nazwa juz wystapila w innym hasle! Wprowadz inna nazwe: " << std::endl;
+                }
+                vec[index][1] = textToChange;
+            }
+            case '3':{
+                // strona www
+                std::cout << "Podaj nazwe strony WWW: " << std::endl;
+                std::cin >> textToChange;
+
+                vec[index][3] = textToChange;
+            }
+            case '4':{
+                // login
+                std::cout << "Podaj login: " << std::endl;
+                std::cin >> textToChange;
+
+                vec[index][4] = textToChange;
+            }
+            default: std::cerr << "void changingValues(): Wybrano niepoprawna opcje!" << std::endl;
+        }
     }
 }
